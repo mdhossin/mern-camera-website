9218e0002a5520bda666f35273d0a1f31014a0e2
md sahadat hossin <mdsahadathossin778@gmail.com>
2022-04-11 19:31:16 +0600

    implemented backend auth route endpoint

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const authCtrl = require("../controllers/authController");
+
+const router = express.Router();
+
+// register route
+router.post("/register", authCtrl.register);
+
+// verify and active account route
+router.post("/active", authCtrl.activeAccount);
+
+// login route
+
+router.post("/login", authCtrl.login);
+
+// logout route must be authenticated
+
+// router.get("/logout", auth, authCtrl.logout);
+
+// refresh token route
+
+router.get("/refresh_token", authCtrl.refreshToken);
+
+// google login route
+router.post("/google_login", authCtrl.googleLogin);
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const connectDb = require("./config/database");
+const errorHandler = require("./middlewares/errorHandler");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+// auth routes
+app.use("/api", authRoutes);
+
+// // user routes
+// app.use("/user", userRoutes);
+
+// Database connection
+connectDb();
+
+// delpoy code
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
+
+// declareing the port here
+const PORT = process.env.PORT || 5000;
+
+// custom error handler
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
